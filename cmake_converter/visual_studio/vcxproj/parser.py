c40e11a862c38e45d050c3be8359b92afb7085a0
Keon Amini <keon.a380@gmail.com>
2022-02-20 21:16:17 -0600

    vcxproj: add support for Image and ResourceCompile includes

--- cmake_converter/visual_studio/vcxproj/parser.py
+++ cmake_converter/visual_studio/vcxproj/parser.py
@@ -126,6 +126,8 @@ class VCXParser(Parser):
             'ClInclude_Include': self.__parse_cl_include_include_attr,
             'None_Include': self.__parse_other_files_include_attr,
             'Text_Include': self.__parse_other_files_include_attr,
+            'Image_Include': self.__parse_other_files_include_attr,
+            'ResourceCompile_Include': self.__parse_other_files_include_attr,
             'Xml_Include': self.__parse_other_files_include_attr,
             'CustomBuild_Include': self.__parse_other_files_include_attr,
             'Condition': self.__parse_condition,
